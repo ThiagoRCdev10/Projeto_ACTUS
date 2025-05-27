4c414224cf7bdd37ce64e7e7f3c0e213bdda81f1
ThiagoRCdev10 <thiagorcdev@gmail.com>
2025-05-26 23:04:23 -0300

    atualizações 9

--- Actus/vite.confg.js
+++ Actus/vite.confg.js
@@ -5,16 +5,16 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: resolve(dirname, 'index.html'),
-        alterar_Login: resolve(dirname, 'alterar_Login.html'),
-        editar_Perfil: resolve(dirname, 'editar_Perfil.html'),
-        esqueceu_Senha: resolve(dirname, 'esqueceu_Senha.html'),
-        insira_Codigo: resolve(dirname, 'insira_Codigo.html'),
-        senha_Nova: resolve(dirname, 'senha_Nova.html'),
-        tela_cadastro: resolve(dirname, 'tela_cadastro.html'),
-        tela_de_Projetos: resolve(dirname, 'tela_de_Projetos.html'),
-        tela_projeto_criacao: resolve(dirname, 'tela_projeto_criacao.html'),
-        tela_projeto_Individual: resolve(dirname, 'tela_projeto_Individual.html'),
+        index: resolve(__dirname, 'index.html'),
+        alterar_Login: resolve(__dirname, 'alterar_Login.html'),
+        editar_Perfil: resolve(__dirname, 'editar_Perfil.html'),
+        esqueceu_Senha: resolve(__dirname, 'esqueceu_Senha.html'),
+        insira_Codigo: resolve(__dirname, 'insira_Codigo.html'),
+        senha_Nova: resolve(__dirname, 'senha_Nova.html'),
+        tela_cadastro: resolve(__dirname, 'tela_cadastro.html'),
+        tela_de_Projetos: resolve(__dirname, 'tela_de_Projetos.html'),
+        tela_projeto_criacao: resolve(__dirname, 'tela_projeto_criacao.html'),
+        tela_projeto_Individual: resolve(__dirname, 'tela_projeto_Individual.html'),
         teste: resolve(__dirname, 'teste.html'),
       }
     }
