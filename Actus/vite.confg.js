dae1df75831c1238dae6e18d17a0734c57bf5e32
ThiagoRCdev10 <thiagorcdev@gmail.com>
2025-05-26 22:59:34 -0300

    atualização 8

--- Actus/vite.confg.js
+++ Actus/vite.confg.js
@@ -1,21 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: 'index.html',
-        alterar_Login: 'alterar_Login.html',
-        editar_Perfil: 'editar_Perfil.html',
-        esqueceu_Senha: 'esqueceu_Senha.html',
-        insira_Codigo: 'insira_Codigo.html',
-        senha_Nova: 'senha_Nova.html',
-        tela_cadastro: 'tela_cadastro.html',
-        tela_de_Projetos: 'tela_de_Projetos.html',
-        tela_projeto_criacao: 'tela_projeto_criacao.html',
-        tela_projeto_Individual: 'tela_projeto_Individual.html',
-        teste: 'teste.html'
+        index: resolve(dirname, 'index.html'),
+        alterar_Login: resolve(dirname, 'alterar_Login.html'),
+        editar_Perfil: resolve(dirname, 'editar_Perfil.html'),
+        esqueceu_Senha: resolve(dirname, 'esqueceu_Senha.html'),
+        insira_Codigo: resolve(dirname, 'insira_Codigo.html'),
+        senha_Nova: resolve(dirname, 'senha_Nova.html'),
+        tela_cadastro: resolve(dirname, 'tela_cadastro.html'),
+        tela_de_Projetos: resolve(dirname, 'tela_de_Projetos.html'),
+        tela_projeto_criacao: resolve(dirname, 'tela_projeto_criacao.html'),
+        tela_projeto_Individual: resolve(dirname, 'tela_projeto_Individual.html'),
+        teste: resolve(__dirname, 'teste.html'),
       }
     }
   }
-})
+});
